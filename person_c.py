7b8cf9e76776e52754c91e68e267cba3d6b5c6ab
Kevin <kevin860612@gmail.com>
2020-07-31 20:44:10 +0800

    multiple people

--- person_c.py
+++ person_c.py
@@ -22,3 +22,17 @@ if __name__ == '__main__':
     p1.speak()
     p1.exercise()
     p1.speak()
+
+    p2 = Person(name='Tim', weight=40, fat_rate=1)
+    p2.eat()
+    p2.eat()
+    p2.speak()
+    p2.exercise()
+    p2.speak()
+
+    p3 = Person(name='Jack', weight=20, fat_rate=2)
+    p3.eat()
+    p3.eat()
+    p3.speak()
+    p3.exercise()
+    p3.speak()
